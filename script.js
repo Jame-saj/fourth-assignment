b36728651c667e987b28f0a1f8987b27af5b704a
Jame-saj <sharminakterjame26@gmail.com>
2026-02-23 13:37:28 +0600

    id call

---
+++ script.js
@@ -0,0 +1,13 @@
+const totalNumber = document.getElementById('totalNum');
+const interNumber = document.getElementById('interviewNum');
+const rejectedNumber = document.getElementById('rejectedNum');
+const jobsNumber = document.getElementById('jobsNum');
+console.log(jobsNumber);
+
+
+
+const btnAll = document.getElementById('btnAll');
+console.log(btnAll);
+const btnInterview = document.getElementById('btnInterview');
+const btnRejected = document.getElementById('btnRejected');
+console.log(btnRejected);
